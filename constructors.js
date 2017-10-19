f3a3645cd7ccafd7bece7b20d630997c32897491
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-19 14:43:20 -0600

    finished all problems

--- constructors.js
+++ constructors.js
@@ -8,7 +8,10 @@
 
 // Code here
 
-
+function CarFactory(make, model){
+  this.make = make;
+  this.model = model;
+}
 
 
 
@@ -27,7 +30,7 @@ function Employee(name, email, hireDate) {
 
 // Code here
 
-
+var bob = new Employee('Bob', 'bob@gmail.com', '01-02-98');
 
 
 
@@ -41,7 +44,16 @@ function Employee(name, email, hireDate) {
 // Write a constructor function, including method definitions, which will make the following function invocations function properly.
 
 // Code here
+function Car(make, model, year){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
 
+  this.moveCar = function(){
+    return this.move += 10;
+  }
+}
 
 
 
